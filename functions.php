9aae089b5d20261b0f90414b3232e6ef5fd06b46
Mattia Migliorini <migliorini.vb.italia@gmail.com>
2014-09-17 12:41:07 +0200

    Update to Melany 1.1.0

--- functions.php
+++ functions.php
@@ -6,6 +6,14 @@
  */
 
 /**
+ * Melany 2viLUG version.
+ *
+ * @since  1.0.0
+ * @var string
+ */
+define( 'MELANY2_VERSION', '1.1.0' );
+
+/**
  * Exclude `planet` category from blog posts index page.
  *
  * @since 1.0.5
@@ -18,12 +26,25 @@ function melany_exclude_category($wp_query) {
 	// Get `planet` category
 	$planet = get_category_by_slug( 'planet' );
 	// Get ID
-	$planet_id = $planet->term_id;
+	if ( $planet ) {
+		$planet_id = $planet->term_id;
+		// Add the category to an array of excluded categories
+		$excluded = array( '-' . $planet_id );
 
-	// Add the category to an array of excluded categories
-	$excluded = array( '-' . $planet_id );
-
-	// Cleaner way to write `$wp_query->set('category__not_in', $excluded);
-	set_query_var( 'category__not_in', $excluded );
+		// Cleaner way to write `$wp_query->set('category__not_in', $excluded);
+		set_query_var( 'category__not_in', $excluded );
+	}
 }
 add_action( 'pre_get_posts', 'melany_exclude_category' );
+
+/**
+ * Enqueue styles.
+ *
+ * @since  1.1.0
+ */
+function melany2_enqueue_styles() {
+	wp_dequeue_style( 'melany-style' );
+	wp_deregister_style( 'melany-style' );
+	wp_enqueue_style( 'melany-2vilug', get_stylesheet_directory_uri() . '/main.css', false, MELANY2_VERSION );
+}
+add_action( 'wp_enqueue_scripts', 'melany2_enqueue_styles', 11 );
